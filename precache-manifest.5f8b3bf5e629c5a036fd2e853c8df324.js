1dfd6f2dfdc41ce8ad55f27193c65309801bead0
abdelfatah.taouala <abdelfatah.taouala@obconsulting.ma>
2019-05-21 10:36:08 +0000

    Updates

---
+++ precache-manifest.5f8b3bf5e629c5a036fd2e853c8df324.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b10be82caaa3799bbb430b60f6f2d3f2",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "b45cae6b0f59b6e67fd3",
+    "url": "/my-resume/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/my-resume/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b45cae6b0f59b6e67fd3",
+    "url": "/my-resume/static/js/main.5ba2e0f0.chunk.js"
+  },
+  {
+    "revision": "2f7b259ab1933d650523",
+    "url": "/my-resume/static/js/runtime~main.1423441b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
