4f2d0ccca88a5c0a0894d1fe0cdd612158c37d3b
abdelfatah.taouala <abdelfatah.taouala@obconsulting.ma>
2019-06-01 00:59:01 +0000

    update modal profile

--- src/views/ModalProfile/ModalProfile.jsx
+++ src/views/ModalProfile/ModalProfile.jsx
@@ -26,25 +26,31 @@ const ModalProfile=(props)=>{
                         <a href="" onClick={e=>{e.preventDefault(); return context.handleShowModal}}>
                             <img src={myPhoto} name="aboutme"  alt="" width="244" height="244" className="img-circle"/>
                         </a>
-                        <h3>Abdelfatah TAOUALA</h3>
+                        <h3>Abdelfatah TAOUALA </h3>
                         <h5>FullStack Developer</h5>
-                        <Badge variant="secondary">Casablanca Morocco</Badge>
-                        <p>
+                        <Badge variant="secondary">Casablanca Maroc</Badge>
+                        </center>
+                        <div>
+                            <div>
+                                <b>Skills : </b>
+                            </div>
+                            <div className="skills"> 
+                                <Badge pill variant="primary">Angular <Badge variant="light">9</Badge></Badge>
+                                <Badge pill variant="warning">React <Badge variant="light">9</Badge></Badge>
+                                <Badge pill variant="primary">Spring Boot <Badge variant="light">9</Badge></Badge>
+                                <Badge pill variant="info">Java 8 <Badge variant="light">9</Badge></Badge>
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <b>Bio :</b>
+                            </div>
+                            <div>
+                                <p>
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam illo quod et quam deleniti quia. Excepturi, vel perferendis natus nihil laborum quisquam. Rem architecto debitis inventore vero veniam aliquam saepe.
                         </p>
-                        <div>
-                            <a >
-                                <img src={angular} name="aboutme" alt="" width="50" height="50" className="img-circle"/>
-                            </a>
-                            <a >
-                                <img src={react} name="aboutme" alt="" width="50" height="50" className="img-circle"/>
-                            </a>
-                            <a >
-                                <img src={spring
-                                } name="aboutme" alt="" width="50" height="50" className="img-circle"/>
-                            </a>
-                        </div>       
-                    </center>
+                            </div>
+                        </div>
                 </div>
             </div>
           
